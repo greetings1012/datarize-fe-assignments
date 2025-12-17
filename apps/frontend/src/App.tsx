297055a7c55d2ed0f7a052f564572ef1d67a09c0
greetings1012 <ahnqwerty@gmail.com>
2025-12-18 02:27:40 +0900

    ♻️ [refactor] Provider 설정 통합

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,27 @@
-import { Dashboard } from './pages/Dashboard/Dashboard';
+import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { theme } from './styles/theme';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { Dashboard } from './pages/Dashboard';
+import styled from '@emotion/styled';
+
+const queryClient = new QueryClient();
+
+const Layout = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 40px 20px;
+`;
 
 export const App = () => {
-  return <Dashboard />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Layout>
+          <Dashboard />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 };
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,21 +1,10 @@
-import { ThemeProvider } from '@emotion/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
-import { theme } from './styles/theme';
-import { GlobalStyles } from './styles/GlobalStyles';
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 );
