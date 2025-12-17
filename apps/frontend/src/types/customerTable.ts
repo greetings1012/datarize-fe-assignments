97e0e983adb2ed35e6db95a726a67948b0360190
greetings1012 <ahnqwerty@gmail.com>
2025-12-18 02:24:58 +0900

    🏷️ [type] 타입 정의 개선
    CustomerTable 관련 타입 분리, DateRange 타입 추가

---
+++ apps/frontend/src/types/customerTable.ts
@@ -0,0 +1,11 @@
+export interface CustomerTableRowData {
+  id: number;
+  name: string;
+  count: number;
+  totalAmount: number;
+}
+
+export interface SelectedCustomer {
+  id: number;
+  name: string;
+}
